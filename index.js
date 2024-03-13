d54f27bd13de71f898374eba68682150c78269d3
ArsenSysyn <92683307+ArsenSysyn@users.noreply.github.com>
2024-03-13 09:25:49 +0000

    some changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const developerInfo = {
     name: "Ian",
     email: "ian@innovative4tea.com"
 };
-
+ 
 // Simulating the registration of a project that aims to digitize traditional tea ceremonies
 const projectInfo = {
     name: "Innovative Tea Ceremony Blockchain Integration",
@@ -18,4 +18,4 @@ const walletAddress = onboardDeveloper(developerInfo);
 const projectId = registerProject(projectInfo);
 receiveTokens(projectId);
 
-console.log("i4-demo-tea project demo completed. Exploring new horizons for tea and technology!");
+console.log("i4-demo-tea project demo completed. Exploring new horizons for tea and technology!!!");
